773a9b392ac887a9cbc24f74294d26dc767be95f
James Neill <jneill542@gmail.com>
2018-03-30 21:33:03 +0100

    Deleted redundant files.

--- normalised_ssd.m
+++
@@ -1,7 +0,0 @@
-function [ ssd ] = NORMALISED_SSD( left, right)
-    norm1 = (left - mean2(left)) / std2(left);
-    norm2 = (right - mean2(right)) / std2(right);
-    diff = norm1 - norm2;
-    ssd = sum(diff(:).^2);
-end
-
--- ssd.m
+++
@@ -1,4 +0,0 @@
-function [ssd] = SSD(left, right)
-    diff = left - right;
-    ssd = sum(diff(:).^2);
-end
